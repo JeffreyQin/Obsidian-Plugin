78a41ed8684cd4ab6f575a36adb5d7b2d0e5f255
JeffreyQin <122770444+JeffreyQin@users.noreply.github.com>
2023-05-15 11:03:22 -0400

    cleared things us

--- main.ts
+++ main.ts
@@ -1,11 +1,6 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, moment, SuggestModal } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-var lastEditDateLine = 8;
-var lastEditDateCh = 13;
 var lastEditDateStr = "updatedDate:"
-
 var dateFormat = "YYYY-MM-DD";
 
 var peopleStr = "people:";
@@ -14,14 +9,13 @@ var peopleListFilePath = "collaborator.md";
 var typeListFilePath = "category.md";
 var suggestionSplitStr = '\n';
 
-interface MyPluginSettings {
-	mySetting: string;
+interface PluginSettings {
+	setting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: PluginSettings = {
+	setting: 'default'
 }
-
 
 // SUGGESTION MODAL
 
@@ -34,9 +28,8 @@ export class SuggestionModal extends SuggestModal<string> {
 		super(app);
 		this.editor = editor;
 		this.suggestionList = suggestionList;
-		
 	}
-
+	
 	getSuggestions(query: string): string[] {
 		return this.suggestionList.filter(
 			(item) => item.toLowerCase().includes(query.toLowerCase())
@@ -68,8 +61,8 @@ export function updateLastEditDate(editor: Editor) {
 	}
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class QuickTextPlugin extends Plugin {
+	settings: PluginSettings;
 
 	async onload() {
 		await this.loadSettings();
@@ -140,30 +133,6 @@ export default class MyPlugin extends Plugin {
 			editor.replaceRange(moment().format(dateFormat), editor.getCursor());
 			updateLastEditDate(editor);
 		});
-	
-
-
-
-
-	
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		/*this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			new Notice('click');
-		});*/
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
@@ -176,50 +145,5 @@ export default class MyPlugin extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
 	}
 }
