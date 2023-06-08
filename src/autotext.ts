eabb610038be979b1060d006ce1584d6793ec45e
Jeffrey Qin <122770444+JeffreyQin@users.noreply.github.com>
2023-06-08 12:01:10 -0400

    Update autotext.ts

--- src/autotext.ts
+++ src/autotext.ts
@@ -1,43 +1,44 @@
-import { App, Editor, moment } from 'obsidian'
+import { App, Editor, moment, Notice } from 'obsidian'
 import { TextPluginSettings } from './settings'
 
-// detect if cursor is currently in the dataview area
+// check if auto text is allowed at current cursor location in current file
 
-export function disabledArea(app: App, editor: Editor, settings: TextPluginSettings): boolean {
-    let numTrack = 0;
-    for (let index = 0; index < editor.getCursor().line - 1; index++) {
-        if (editor.getLine(index).startsWith(settings.dataviewHeaderLine)) {
-            numTrack ++;
+export function disableAutoText(app: App, editor: Editor, settings: TextPluginSettings): boolean {
+    if (!editor.getLine(0).startsWith(settings.separationLineStr)) {
+        return true;
+    } 
+    let dataviewLineTrack = 0;
+    let isTemplate = false;
+    for (let index = 1; index < editor.getCursor().line; index++) {
+        let line = editor.getLine(index);
+        if (line.startsWith(settings.separationLineStr)) {
+            return true;
+        }
+        if (line.startsWith(settings.dataviewHeaderLine)) {
+            dataviewLineTrack ++;
+        }
+        if (line.startsWith(settings.templateDetectionStr)) {
+            isTemplate = true;
         }
     }
-    if (numTrack == 1) {
+    if (dataviewLineTrack <= 1 || !isTemplate) {
         return true;
     }
-    if (numTrack == 0 && (editor.getLine(editor.getCursor().line).startsWith(settings.dataviewHeaderLine)
-        || editor.getLine(editor.getCursor().line - 1).startsWith(settings.dataviewHeaderLine))) {
-            return true;
-    }
     return false;
 }
 
-// check if the cursor is currently in the newest date 
-
-export function notNewestDate(app: App, editor: Editor, settings: TextPluginSettings): boolean {
-    for (let index = 0; index < editor.getCursor().line; index++) {
-        if (editor.getLine(index).startsWith(settings.separationLineStr)) {
-            return true;
-        }
-    }
-    return false;
-}
 
 //generate auto text (date + username)
 
 export function generateAutoText(app: App, editor: Editor, settings: TextPluginSettings) {
-    if (!disabledArea(app, editor, settings) && !notNewestDate(app, editor, settings)) {
+    if (!disableAutoText(app, editor, settings)) {
         editor.replaceRange(
             '\n\n' + settings.separationLineStr + '\n' + moment().format(settings.dateFormat) + ' ' + settings.username,
-            { line: editor.getCursor().line - 1, ch: 0 }
+            { line: editor.getCursor().line - 1, ch: editor.getLine(editor.getCursor().line - 1).length }
+        )
+        editor.replaceRange(
+            '\n',
+            { line: editor.getCursor().line + 1, ch: 0}
         )
     }
 }
